9dd1cd17a43223434dc7e00cd163613f934aa047
Benoit Petit <bpetit@hubblo.org>
2022-10-04 16:19:57 +0200

    removed useless var

--- stats.sh
+++ stats.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-PUSHGATEWAY_ENDPOINT="http://localhost:9091"
 JOB_NAME="containers_stats"
 export LC_NUMERIC=POSIX
 
@@ -9,6 +8,7 @@ public_ip_address=$(host myip.opendns.com resolver1.opendns.com | grep "myip.ope
 while [ true ]
 do
 #cat << EOF | curl --data-binary @- ${PUSHGATEWAY_ENDPOINT}/metrics/job/${JOB_NAME}/instance/${public_ip_address}
-  echo -e "HTTP/1.1 200 OK\n\n$(docker stats --no-stream | grep -v CONTAINER | awk -f docker_stats_to_prometheus.awk)" | nc -l -k -p 8089;
+  #echo -e "HTTP/1.1 200 OK\n\n$(docker stats --no-stream | grep -v CONTAINER | awk -f docker_stats_to_prometheus.awk)" | nc -l -k -p 8089;
+  echo -e "HTTP/1.1 200 OK\n\n$(docker stats --no-stream | grep -v CONTAINER | awk -f docker_stats_to_prometheus.awk)" | nc -l -k -p ${STATS_TCP_PORT:-8089} -q 1;
 #EOF
 done
